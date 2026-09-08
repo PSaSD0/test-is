aad5d884bae6ca3e97a516ed2ec6045d317312ef
PSaSD0 <maksponom07@mail.ru>
2026-09-08 23:13:26 +0400

    добавлено окно ввода

--- main.py
+++ main.py
@@ -3,17 +3,46 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from tkinter import *
+from tkinter import messagebox
 
-driver = webdriver.Chrome()
-driver.get("https://www.wikipedia.org")
+def test():
+    driver = webdriver.Chrome()
+    driver.get("https://www.wikipedia.org")
 
-search = driver.find_element(By.ID, "searchInput")
-search.send_keys("Россия")
-search.send_keys(Keys.ENTER)
+    search = driver.find_element(By.ID, "searchInput")
+    search.send_keys(en_text.get())
+    search.send_keys(Keys.ENTER)
 
-wait = WebDriverWait(driver, 10)
-heading = wait.until(expected_conditions.presence_of_element_located((By.ID, "firstHeading")))
-assert "Россия" in heading.text
+    wait = WebDriverWait(driver, 10)
+    
+    heading = wait.until(expected_conditions.presence_of_element_located((By.ID, "firstHeading")))
+    
+    if en_text.get().lower() in heading.text.lower():
+        messagebox.showinfo("Атотест","Тест прошёл успешно!")
+    else:
+        messagebox.showinfo("Автотест", "Тест не пройден!")
 
-print("Тест прошёл успешно!")
-driver.quit()
+    driver.quit()
+
+window = Tk()
+
+window.title("Автотест википедии")
+window.geometry('400x300')
+
+frame = Frame(window, padx=10, pady=10)
+frame.pack(expand=True)
+
+lb = Label(frame, text = "Автотест википедии")
+lb.grid(row=1, column=1, pady=10)
+
+lb_text = Label(frame, text = "Какого слова вы хотите произвести автотест?")
+lb_text.grid(row=3, column=1)
+
+en_text = Entry(frame)
+en_text.grid(row=4, column=1)
+
+btn = Button(frame, text="Поиск", command=test)
+btn.grid(row=5, column=1, pady=5)
+
+window.mainloop()
